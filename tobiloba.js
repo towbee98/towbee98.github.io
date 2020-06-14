541994e5707ee61a6d28ac04f0741d7c47d99e7d
towbee98 <tobiemma200@gmail.com>
2020-06-14 01:20:17 +0100

    Updated the portfolio site

--- tobiloba.js
+++ tobiloba.js
@@ -2,21 +2,34 @@ $dropdown=document.querySelector(".dropdown");
 $toggleBtn=document.querySelector(".toggle-btn");
 $backdrop=document.querySelector(".backdrop");
 $dropbtn=document.querySelectorAll(".dropdown a");
-
+clicks=0;
+clickedDropdownOptions=false;
 $toggleBtn.addEventListener("click",call);
 function call(){
-    $dropdown.style["display"]="block";
-    $backdrop.style["display"]="block";
-    $backdrop.addEventListener("click",remove);
-    for(i=0;i<$dropbtn.length;i++){
-    $dropbtn[i].addEventListener("click",remove);
+    clicks++;
+    if(clicks%2===1){
+        $dropdown.style["opacity"]="1";
+        $dropdown.style["transform"]="translateX(0rem)";
+        $backdrop.style["opacity"]="0.7";
+        for(i=0;i<$dropbtn.length;i++){
+            $dropbtn[i].addEventListener("click",remove);
+        }
     }
-  
+    else{
+        $dropdown.style["opacity"]="0";
+    $dropdown.style["transform"]="translateX(20rem)";
+    $backdrop.style["opacity"]="0";
+    }
+    
 }
 function remove(){
-    $dropdown.style["display"]="none";
-    $backdrop.style["display"]="none";
+    clickedDropdownOptions=true;
+    if(clickedDropdownOptions){ 
+    $dropdown.style["opacity"]="0";
+    $dropdown.style["transform"]="translateX(20rem)";
+    $backdrop.style["opacity"]="0";
+    clicks++;
+    clickedDropdownOptions=false;
+    }
 }
-
-
 
